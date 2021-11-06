425eb6bc66d1a5fa28defdde72dadbb72898675c
Obumuneme Nwabude <obumnwabude@gmail.com>
2021-11-06 11:06:40 +0100

    create Todo class and implement Todo UI

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,19 @@ void main() {
   runApp(TodoPage());
 }
 
+class Todo {
+  bool completed = false;
+  String content = 'Deliver today\'s talk';
+}
+
 class TodoPage extends StatefulWidget {
   @override
   _TodoPageState createState() => _TodoPageState();
 }
 
 class _TodoPageState extends State<TodoPage> {
+  List<Todo> todos = [Todo(), Todo(), Todo()];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,10 +38,24 @@ class _TodoPageState extends State<TodoPage> {
               IconButton(onPressed: () {}, icon: Icon(Icons.dark_mode))
             ],
           ),
-          body: Center(
-            child: Opacity(
-                opacity: 0.5, child: Image.asset('assets/images/empty.png')),
-          ),
+          body: todos.isEmpty
+              ? Center(
+                  child: Opacity(
+                      opacity: 0.5,
+                      child: Image.asset('assets/images/empty.png')),
+                )
+              : ListView.builder(
+                  padding: EdgeInsets.symmetric(vertical: 16),
+                  itemCount: todos.length,
+                  itemBuilder: (context, i) {
+                    return ListTile(
+                        leading: Icon(Icons.radio_button_unchecked_outlined),
+                        title: Text(
+                          todos[i].content,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        trailing: Icon(Icons.horizontal_rule_outlined));
+                  }),
           floatingActionButton: FloatingActionButton(
             child: Icon(Icons.add),
             onPressed: () {},
